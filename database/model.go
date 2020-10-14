72af7105ac1ce38923462943a54202704ba1d6e9
erdaltsksn <erdal@taskesen.me>
2020-10-14 23:31:15 +0300

    feat: Add BaseModel for gorm entities

---
+++ database/model.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+// BaseModel is a model definition, including fields `ID` as UUID, `CreatedAt`,
+// `UpdatedAt`, and `DeletedAt`, which could be embedded in your models.
+//
+// USAGE:
+// 	type User struct {
+// 		database.BaseModel
+//
+// 		Email    string `json:"email" gorm:"UNIQUE;NOT NULL"`
+// 		Password string `json:"password" gorm:"NOT NULL"`
+// 	}
+type BaseModel struct {
+	ID string `json:"id" gorm:"PRIMARY_KEY"`
+
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at" sql:"index"`
+}
+
+// BeforeCreate generates and sets an `UUID` for `ID` column.
+func (baseModel *BaseModel) BeforeCreate(tx *gorm.DB) error {
+	generatedUUID, err := uuid.NewRandom()
+	if err != nil {
+		return err
+	}
+	baseModel.ID = generatedUUID.String()
+
+	// Everything is fine
+	return nil
+}
